0985afb7ba2f2c559ded31de8ec09d8df4a3c11f
Tercio Gaudencio Filho <terciofilho@gmail.com>
2024-11-06 23:22:33 -0300

    Removed old code, inverted LEDs.

--- src/led.c
+++ src/led.c
@@ -15,15 +15,23 @@ void task_led(void *arg)
     gpio_init(LED_PIN_ACT);
     gpio_set_dir(LED_PIN_ACT, GPIO_OUT);
 
+    gpio_init(LED_PIN_BUILDIN);
+    gpio_set_dir(LED_PIN_BUILDIN, GPIO_OUT);
+
+    gpio_xor_mask(1u << LED_PIN_BUILDIN);
     while (true)
     {
         // Act LED
         if (uart_activity)
         {
             uart_activity = false;
+
             gpio_xor_mask(1u << LED_PIN_ACT);
+            gpio_xor_mask(1u << LED_PIN_BUILDIN);
             vTaskDelay(pdMS_TO_TICKS(LED_ACT_DELAY / 2));
+
             gpio_xor_mask(1u << LED_PIN_ACT);
+            gpio_xor_mask(1u << LED_PIN_BUILDIN);
             vTaskDelay(pdMS_TO_TICKS(LED_ACT_DELAY / 2));
         }
         else
